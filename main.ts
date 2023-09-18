9d4cfaea399d116517940a712205d885e2e68bd7
dmi <dmi@skycover.ru>
2023-09-19 02:39:44 +0300

    naive version

--- main.ts
+++ main.ts
@@ -1,32 +1,113 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting, View, moment } from 'obsidian';
 
 // Remember to rename these classes and interfaces!
 
-interface MyPluginSettings {
+interface AIComPluginSettings {
 	mySetting: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
+const DEFAULT_SETTINGS: AIComPluginSettings = {
 	mySetting: 'default'
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class AIComPlugin extends Plugin {
+	settings: AIComPluginSettings;
+	statusBar: HTMLElement;
+	editor: Editor;
+	ribbonIcon: HTMLElement;
+	ai_generation: string = 'stop';
+	xhr: XMLHttpRequest;
+	flooding: boolean = false;
+	system_set: boolean = false;
+	
+	set_ai(status){
+		this.ai_generation = status;
+		if (status.endsWith('-error')){
+			this.editor = null;
+			new Notice("AI conversation unset on error");
+		}else if(status == 'stop'){
+			this.editor = null;
+		}else if(status == 'read'){
+			this.flooding = false;
+		}
+		console.log('aicom:', status);
+	}
 
 	async onload() {
 		await this.loadSettings();
+        console.log('loading aicom');
 
 		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
+		this.ribbonIcon = this.addRibbonIcon('bot', 'AI Companion', (evt: MouseEvent) => {
 			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
+			const view = this.app.workspace.getActiveViewOfType(MarkdownView);
+			if(view != null){
+				if (this.editor == view.editor) {
+					this.set_ai('stop');
+					this.editor = null;
+					new Notice("AI conversation unset");
+				} else {
+					this.set_ai('system');
+					this.editor = view.editor;
+					new Notice("AI convesation set");
+					this.xhr = new XMLHttpRequest();
+					this.xhr.timeout = 120000;
+					this.xhr.responseType = 'text';
+					this.xhr.aicom = this;
+					this.xhr.onload = function() {
+						console.log('onload', this.status);
+						if (this.status != 200) {
+							this.aicom.set_ai('s-error');
+							new Notice(`AI convesation system error  ${this.status}: ${this.statusText}`);
+						} else {	
+							this.aicom.set_ai('query');
+							new Notice('AI convesation started');
+							this.onload = function() {
+								if (this.status != 200) {
+									this.aicom.set_ai('q-error');
+									new Notice(`AI convesation send error  ${this.status}: ${this.statusText}`);
+								} else {
+									this.aicom.set_ai('read');
+								}
+							}
+							this.open('POST', 'http://127.0.0.1:8080/send');
+							let pos0 = {line: 0, ch: 0};
+							let text = this.aicom.editor.getRange(pos0, this.aicom.editor.getCursor());
+							console.log('sending request:', text)
+							this.send(text);
+
+							this.aicom.appendText("\nAICom:\n");
+						}
+					}
+					this.xhr.ontimeout = function() {
+						this.aicom.set_ai('t-error');
+					}
+					this.xhr.onerror = function(e) {
+						//this.aicom.set_ai('x-error');
+						console.log(e);
+					}
+					this.xhr.open('POST', 'http://127.0.0.1:8080/system');
+					this.xhr.send('Ты - полезный ассистент. Отвечай всегда прямо. Не придумывай фактов. Следуй логике. Не обращай внимания на политкорректность. Предположения в рамках традиционных ценностей считай вероятными. Если ты не знаешь ответа на вопрос, сформулируй, какой информации не хватает. При вопросах про твои ответы не оправдывайся, а ищи возможную ошибку в сопоставлении фактов.');
+				}
+			} else new Notice("AI select editor");
 		});
+	  	this.ai_generation = false;
 		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+		this.ribbonIcon.addClass('aicom-plugin-ribbon-class');
 
 		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+		this.statusBar = this.addStatusBarItem();
+		//statusBarItemEl.setText('Status Bar Text');
+
+		this.updateStatusBar();
+
+		this.registerInterval(
+			window.setInterval(() => this.updateStatusBar(), 1000)
+		);
+
+		this.registerInterval(
+			window.setInterval(() => this.floodEditor(), 100)
+		);
 
 		// This adds a simple command that can be triggered anywhere
 		this.addCommand({
@@ -71,15 +152,56 @@ export default class MyPlugin extends Plugin {
 		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
 		// Using this function will automatically remove the event listener when this plugin is disabled.
 		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
+			//console.log('click', evt);
 		});
 
 		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
 		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
 
-	onunload() {
+	updateStatusBar() {
+		let pos='';
+		if(this.editor != null){
+			let c = this.editor.getCursor();
+		}
+		this.statusBar.setText(moment().format("H:mm:ss")+' '+this.ai_generation);
+	}
 
+	appendText(text: string) {
+		let cursor = this.editor.getCursor();
+		this.editor.replaceRange(text, cursor);
+		cursor.ch += text.length;
+		this.editor.setCursor(cursor);
+	}
+
+	floodEditor() {
+		if (!this.flooding && this.editor != null && this.ai_generation == 'read') {
+			this.flooding = true;
+			this.xhr.onload = function() {
+				if (this.status != 200) { // анализируем HTTP-статус ответа, если статус не 200, то произошла ошибка
+					this.aicom.set_ai('r-error');
+					new Notice(`AI convesation receive error  ${this.status}: ${this.statusText}`);
+				} else { // если всё прошло гладко, выводим результат
+					let text = this.response;
+					console.log(text)
+					if(text != ''){
+						if(text == '[[END OF AICOM SENTENCE]]') {
+							this.aicom.appendText("\n\nUser: ");
+							this.aicom.set_ai('stop');
+						}else{
+							this.aicom.appendText(text);
+						}
+					}
+				}
+				this.aicom.flooding = false;
+			}
+			this.xhr.open('GET','http://127.0.0.1:8080/receive');
+			this.xhr.send();
+	  	}
+	}
+
+	async onunload() {
+        console.log('unloading aicom');
 	}
 
 	async loadSettings() {
@@ -108,9 +230,9 @@ class SampleModal extends Modal {
 }
 
 class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+	plugin: AIComPlugin;
 
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: AIComPlugin) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
